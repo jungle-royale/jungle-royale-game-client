c3f2cc33d1b748a4f1ba11ec96a0f9e2cd90f3db
Sro01 <sara0099@naver.com>
2025-01-17 16:14:04 +0900

    fix: 보기 전환 버튼 버그 수정
    - 원인: MainCanvas와 WatchMode Canvas의 Sort Order 때문
    - 해결: MainCanvas의 자식 객체로 WatchMode 객체 생성
    	- 해당 객체를 활성화/비활성화

--- Assets/Scripts/Manager/InGameGUI/NextButton.cs
+++ Assets/Scripts/Manager/InGameGUI/NextButton.cs
@@ -4,35 +4,54 @@ using UnityEngine.UI;
 
 public class NextButton : MonoBehaviour
 {
-    void Start()
+    // public GameObject quitButtonObject;
+
+    private void OnEnable()
     {
-        // QuitButton GameObject를 찾음 (버튼이 있는 객체)
-        GameObject quitButtonObject = GameObject.Find("NextButton");
+        // 현재 활성화된 Canvas의 자식에서 GuideToggleButton 찾음
+        Button[] nextButtons = GetComponentsInChildren<Button>(true);
 
-        if (quitButtonObject != null)
+        foreach (Button nextButton in nextButtons)
         {
-            // Button 컴포넌트를 가져옴
-            Button quitButton = quitButtonObject.GetComponent<Button>();
-
-            if (quitButton != null)
+            if (nextButton.name == "NextButton")
             {
-                // OnClick 이벤트에 메서드 추가
-                quitButton.onClick.AddListener(() => WatchNext());
+                // 기존 리스너를 제거하고 새로 추가
+                nextButton.onClick.RemoveAllListeners();
+                nextButton.onClick.AddListener(WatchNext);
             }
-            else
-            {
-                Debug.LogError("Button component is missing on NextButton GameObject.");
-            }
-        }
-        else
-        {
-            Debug.LogError("NextButton GameObject not found.");
         }
     }
 
+    // void Start()
+    // {
+    //     // QuitButton GameObject를 찾음 (버튼이 있는 객체)
+    //     if (quitButtonObject == null)
+    //         quitButtonObject = GameObject.Find("NextButton");
+
+    //     if (quitButtonObject != null)
+    //     {
+    //         // Button 컴포넌트를 가져옴
+    //         Button quitButton = quitButtonObject.GetComponent<Button>();
+
+    //         if (quitButton != null)
+    //         {
+    //             // OnClick 이벤트에 메서드 추가
+    //             quitButton.onClick.AddListener(() => WatchNext());
+    //         }
+    //         else
+    //         {
+    //             Debug.LogError("Button component is missing on NextButton GameObject.");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("NextButton GameObject not found.");
+    //     }
+    // }
+
     public void WatchNext()
     {
-        // Debug.Log("Next Button");
+        // Debug.Log("Next Button 클릭");
         CameraManager cameraManager = GameObject.FindFirstObjectByType<CameraManager>();
         if (cameraManager == null)
         {
